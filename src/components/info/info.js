fecf616601458e51624268f48e3251e98cd9bc4f
Linh Nguyen <linhnguyen@linhs-macbook-pro.home>
2024-06-15 22:31:11 +0200

    update about text animation

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './info.css';
 import Break from '@/app/break/break';
-
+import Text1 from '@/components/info/text1/text1';
+import Text2 from '@/components/info/text2/text2';
+import Text3 from '@/components/info/text3/text3';
 
 const Info = () => {
   return (
@@ -15,7 +17,7 @@ const Info = () => {
           <h1>INTRODUCTION</h1>
         </div>
         <div className="bigbox">
-          <p>For over 3 years, I&apos;ve been on an exciting digital design journey, working with both new startups and established corporations. To me, websites are more than just designs; they tell the story of the product and how it helps users. I love exploring unique and creative interactions, but what I care about also is making sure the handoff to developers is smooth and easy.</p>    
+        <Text1/>
         </div>
       </div>
 
@@ -25,9 +27,7 @@ const Info = () => {
           <h1>EXPERIENCE</h1>
         </div>
         <div className="bigbox">
-          <p>
-            TBWA Group <span className="point"> * </span> Phibious Group <span className="point"> * </span> Navigos Group<span className="point"> * </span> Amex GBT <span className="point"> * </span> Egerie 
-          </p>
+        <Text2/>
         </div>
       </div>
 
@@ -37,9 +37,7 @@ const Info = () => {
           <h1>SKILLS</h1>
         </div>
         <div className="bigbox">
-          <p>
-            User Research <span className="point"> * </span> Visual Design <span className="point"> * </span> Usability Testing <span className="point"> * </span> Front-End Development <span className="point"> * </span> Prototyping 
-          </p>
+        <Text3/>
         </div>
       </div>
  
